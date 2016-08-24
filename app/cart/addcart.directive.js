cacf13ff0eed44b773e45dc3f9ce35dc5135f8cb
Piotr <ozrabal@gmail.com>
2016-08-24 16:12:08 +0200

    bckp commit

--- app/cart/addcart.directive.js
+++ app/cart/addcart.directive.js
@@ -2,18 +2,24 @@ define(['angular', 'app'], function(angular, app){
     angular.module('app')
         .directive('addToCart', function(){
         return{
-            restrict: 'A',
-
+            restrict: 'E',
+            templateUrl:'app/cart/addtocart.html',
             scope: {
                 item: '='
             },
-
+            //transclude: true,
             link: function(scope, element, attributes){
                 //scope.$watch('item', function(item) {
                 element.bind('click', function(){
                         console.log(scope.item);
 
                     });
+//todo called twice??
+                scope.itemInCart = function(){
+                    console.log('incart' + scope.item.id);
+                    return true;
+                }
+
                 //})
             }
         }
